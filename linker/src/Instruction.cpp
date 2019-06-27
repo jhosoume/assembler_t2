6860868dbf6505ce5f527d12f1f05800788e0766
Juliana Hosoume <ju.hosoume@gmail.com>
2019-06-27 19:03:49 -0300

    Minor fix

--- linker/src/Instruction.cpp
+++ linker/src/Instruction.cpp
@@ -1,5 +1,5 @@
 #include "Instruction.hpp"
 
 Instruction::Instruction(string name, int n_op, int size, int op_code)
-  : name{name}, num_operands{n_op}, size{size} op_code{op_code}
+  : name{name}, num_operands{n_op}, size{size}, op_code{op_code}
   {}
